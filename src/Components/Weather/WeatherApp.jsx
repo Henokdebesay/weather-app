cb83581a4c3db1c6808fa4232bd89067a130f65d
Henok Debesay <henokdebesay@gmail.com>
2024-03-18 08:25:04 -0700

    fixed a bug

--- src/Components/Weather/WeatherApp.jsx
+++ src/Components/Weather/WeatherApp.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import './Weather/Weather.css'
+import './WeatherApp.jsx'
 
-import clear from "./clear.png"
-import cloud from "./cloud.png"
-import drizzle from "./drizzle.png"
-import humidity from "./humidity.png"
-import rain from "./rain.png"
-import search from "./search.png"
-import snow from "./snow.png"
-import wind from "./wind.png"
+// import clear from "/Users/macuser/Documents/github/weather-app/src/assets/clear.png"
+// import cloud from "/Users/macuser/Documents/github/weather-app/src/assets/cloud.png"
+// import drizzle from "/Users/macuser/Documents/github/weather-app/src/assets/drizzle.png"
+// import humidity from "/Users/macuser/Documents/github/weather-app/src/assets/humudity.png"
+// import rain from "/Users/macuser/Documents/github/weather-app/src/assets/rain.png"
+// import search from "/Users/macuser/Documents/github/weather-app/src/assets/search.png"
+// import snow from "/Users/macuser/Documents/github/weather-app/src/assets/snow.png"
+// import wind from "/Users/macuser/Documents/github/weather-app/src/assets/wind.png"
 
 
 function WeatherApp() {
   return (
-    <div>WeatherApp</div>
+    <>
+           <div>WeatherApp</div>
+           <img src={cloud} alt="" />
+
+    </>
+ 
   )
 }
 
